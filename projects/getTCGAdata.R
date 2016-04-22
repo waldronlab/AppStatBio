dce86834ad66af5eee6566c5dba926bbd76013ab
Levi Waldron <lwaldron.research@gmail.com>
2016-04-22 14:31:22 +0200

    Organization.

--- projects/getTCGAdata.R
+++ projects/getTCGAdata.R
@@ -1,7 +1,7 @@
 if( !require(RTCGAToolbox) ){
-    library(devtools)
-    install_github("LiNk-NY/RTCGAToolbox")
-    library(RTCGAToolbox)
+  library(BiocInstaller)
+  biocLite("LiNk-NY/RTCGAToolbox")
+  library(RTCGAToolbox)
 }
 
 all.dates <- getFirehoseRunningDates()
@@ -33,6 +33,20 @@ for (i in 1:length(tcga.res)) {
   eset = try(extract(tcga.res[[i]], "RNAseq2_Gene_Norm"))
   if (is(eset, "try-error"))
     next
-  eset = eset[, substr(sampleNames(eset), 14, 15) %in% c("01", "03", "09")]
+  eset = eset[, as.integer(substr(sampleNames(eset), 14, 15)) %in% 1:9]
   saveRDS(eset, file = paste0(names(tcga.res)[i], "_RNAseq2.rds"))
 }
+
+res <- getFirehoseData("SKCM", runDate="20151101", RNAseq2_Gene_Norm=TRUE)
+eset = extract(res, "RNAseq2_Gene_Norm")
+devtools::session_info()
+
+expdat <- res@RNASeq2GeneNorm
+colnames(expdat) <- tolower(substr(colnames(expdat), 1, 12))
+clindat <- res@Clinical
+rownames(clindat) <- gsub(".", "-", rownames(clindat), fixed=TRUE)
+clindat <- clindat[rownames(clindat) %in% colnames(expdat), ]
+expdat <- expdat[, match(rownames(clindat), colnames(expdat))]
+
+all.equal(colnames(expdat), rownames(clindat))
+eset <- ExpressionSet(assayData=expdat, phenoData=AnnotatedDataFrame(clindat))
