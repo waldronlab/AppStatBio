0a94b114b46035d72d5c45d6a23195a0acb14e60
Simonedet <detassis.simone@gmail.com>
2017-06-20 13:07:48 +0200

    Add files via upload

---
+++ Day1/labs/startingex_Simone.R
@@ -0,0 +1,11 @@
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv"
+filename <- "femaleMiceWeights.csv" 
+download(url, destfile=filename)
+data <- read.csv(filename)
+data[12,2]
+data$Bodyweight[11]
+length(data$Diet)
+mean(data[c(13:20),2])
+set.seed(1)
+data[sample(13:24,1),2]
